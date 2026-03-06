1cd36af85158b66afdc0fb9257dd7ccb606dec81
Aayush Kashyap <33807516+TheKing0x9@users.noreply.github.com>
2026-03-07 02:12:27 +0530

    Remove images configuration from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  images: {
-    unoptimized: true,
-  },
   basePath: process.env.PAGES_BASE_PATH,
 };
 
